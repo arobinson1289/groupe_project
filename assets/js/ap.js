53ab65a96ce1d62e171be31de7a4cba2ec5e9551
Brad Cas <bmcasalvieri@gmail.com>
2019-04-11 13:59:25 -0400

    ajax calls for musixmatch and urban dictionary

--- assets/js/ap.js
+++ assets/js/ap.js
@@ -1,7 +1,9 @@
-// pull search input from urban dictionary form 
-var search = "zomg";
+// URBAN DICTIONARY AJAX CALL
 
-var queryURL = "https://urbanscraper.herokuapp.com/search/" + search;
+// pull search input from urban dictionary form 
+var search = "hit a lick";
+
+var queryURL = "https://alex-rosencors.herokuapp.com?url=https://urbanscraper.herokuapp.com/search/" + search;
 
 $.ajax({
   url: queryURL,
@@ -13,26 +15,94 @@ $.ajax({
   for (var i = 0; i < response.length; i++) {
     
     // create a definition div
-    $definitionDiv = $("<div>")
+    $definitionCard = $("<div>")
+      .addClass("card")
+
+    $cardBody = $("<div>")
+      .addClass("card-body")
+
+    // grab term
+    $term = $("<h4>")
+      .addClass("card-title")
+      .text(search)
 
     // grab the definition
     var $definition = $("<h5>")
-      .addClass("definition")
-      .text(i + "Definition: " + response[i].definition);
+      .addClass("card-subtitle definition")
+      .text((i + 1) + ")   Definition: " + response[i].definition);
 
     // grab example
     var $example = $("<p>")
-      .addClass("example")
+      .addClass("card-text example")
       .text("Example: " + response[i].example);
 
     // append to $definitionDiv
-    $definitionDiv.append($definition, $example);
+    $definitionCard.append($term, $definition, $example);
 
     // append $definitionDiv to page
-    $("#").append($definitionDiv);
+    $("#definitions").append($definitionCard);
 
-    console.log($definitionDiv)
 
   };
 
 });
+
+//ajax call for musixmatch song title
+var song = "6 foot 7 foot";
+
+var apiKey = "f64fe601571e8c26f4b0845395793cff";
+
+var queryUrl = "https://alex-rosencors.herokuapp.com?url=https://api.musixmatch.com/ws/1.1/track.search?q_track=" + song + "&s_track_rating=desc&page_size=25&apikey=" + apiKey
+
+console.log(queryUrl)
+
+$.ajax ({
+  url: queryUrl,
+  method: "GET"
+}).then(function(response){
+
+  console.log(response)
+
+  var trackList = response.message.body.track_list;
+  console.log(trackList)
+
+  for (var i = 0; i < trackList.length; i++){
+    var $songBtn = $("<button>")
+      .addClass("btn btn-primary col-12 mb-1 song-button")
+      .attr("data-trackId", trackList[i].track.track_id)
+    
+    var $songName = $("<p>")
+      .addClass("m-0")
+      .text(`"${trackList[i].track.track_name}"`)
+      
+    var $artistName = $("<p>")
+      .addClass("m-0")
+      .text(trackList[i].track.artist_name)
+
+    $songBtn.append($songName, $artistName)
+    $("#button").append($songBtn)
+
+  }
+
+});
+
+$(document).on("click", ".song-button", function(event) {
+  
+  event.preventDefault();
+
+  $("#lyrics").empty()
+
+  var trackId = $(this).attr("data-trackId");
+  console.log(trackId)
+
+  var queryURL = "https://alex-rosencors.herokuapp.com?url=https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + trackId + "&apikey=f64fe601571e8c26f4b0845395793cff"
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+    var $lyrics = $("<p>").text(response.message.body.lyrics.lyrics_body).appendTo($("#lyrics"))
+  })
+})
